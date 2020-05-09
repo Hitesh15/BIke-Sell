3f4efae44a3ca5e7a38131648a8bd8207c0e7f68
Hitesh15 <47603132+Hitesh15@users.noreply.github.com>
2020-05-09 17:30:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
